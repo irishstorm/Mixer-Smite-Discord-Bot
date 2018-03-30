73f43118f62343c53919ca9779f9147010c77664
irishstorm <ccullen15@gmail.com>
2018-03-30 11:38:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,10 +17,6 @@ bot.login(token);
 //  take it out and store one instance in a variable
 //  Push the code to an new array
 //  check to make sure the code is not already added
-while(1){
-    getChatMessages();
-}
-
 bot.on("ready", () => {
     getChatMessages();
 })
