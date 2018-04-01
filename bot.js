5f4f26597ef26ed347b86b66afc7481c593ae619
irishstorm <ccullen15@gmail.com>
2018-04-01 06:39:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,37 @@
-const commando = require('discord.js-commando');
-const bot = new commando.Client({ unknownCommandResponse: false });
-const Mixer = require('beam-client-node');
-const mixerClient = new Mixer.Client(new Mixer.DefaultRequestRunner());
-
-//   process.env.BOT_TOKEN
-let token = process.env.BOT_TOKEN;
-let channelId = 19088261;  //SmiteGame
-let chatMessages = [];
-let codeList = [];
-
-bot.login(token);
-
 //  ToDo:
 //  Read in chatlog and push it to an array.
 //  Find the code in the chatlog
 //  take it out and store one instance in a variable
 //  Push the code to an new array
 //  check to make sure the code is not already added
+
+const commando = require('discord.js-commando');
+const bot = new commando.Client({ unknownCommandResponse: false });
+const Mixer = require('beam-client-node');
+const mixerClient = new Mixer.Client(new Mixer.DefaultRequestRunner());
+
+//   process.env.BOT_TOKEN - Heroku
+let token = process.env.BOT_TOKEN;
+let channelId = 19088261;  //SmiteGame 19088261
+let chatMessages = [];
+let codeList = [];
+codePrefex = ["APMCB", "APMCW", "APYSC"];
+
+bot.login(token);
+
+var guild;
+var channel;
+
+bot.on('ready', function () {
+    var guilds = bot.guilds;
+
+    guilds.forEach(function (g) {
+        guild = g;
+    });
+
+    channel = guild.channels.get("429022868140195842");
+});
+
 bot.on('message', message => {
     if (message.content === '!code') {  
         message.channel.send("/claimpromotion " + codeList[codeList.length - 1]);
@@ -25,8 +40,9 @@ bot.on('message', message => {
 
 bot.on('message', message => {
     if (message.content === '!allcodes') {  
-        for (var i = 0; i < codeList.length; i++)
+        for (var i = 0; i < codeList.length; i++){
             message.channel.send(codeList[i] + " ");
+        }
     }
 });
 
@@ -38,27 +54,21 @@ function getChatMessages(){
         for (var i = 0; i < res.body.length; i++){
             chatMessages.push(res.body[i].message.message[0].text);
         }
-        findCodes(chatMessages);
+        findCode(chatMessages);
     })
     .catch(error => { console.error(error); });
 }
 
-function findCodes(arrayObj){
+function findCode(arrayObj){
     arrayObj.map((res) => {
-        if(res.includes("APMCB")){
-            let temp = res.slice(res.indexOf("APMCB"), res.indexOf("APMCB") + 17);
-            if(!codeList.includes(temp))
-                codeList.push(temp);
-        }
-        else if(res.includes("APMCW")){
-            let temp = res.slice(res.indexOf("APMCW"), res.indexOf("APMCW") + 17);
-            if(!codeList.includes(temp))
-                codeList.push(temp);
-        }
-        else if(res.includes("APYSC")){
-            let temp = res.slice(res.indexOf("APYSC"), res.indexOf("APYSC") + 17);
-            if(!codeList.includes(temp))
-                codeList.push(temp);
+        for (var i = 0; i < codePrefex.length; i++){
+            if(res.includes(codePrefex[i])){
+                let temp = res.slice(res.indexOf(codePrefex[i]), res.indexOf(codePrefex[i]) + 17);
+                if(!codeList.includes(temp)){
+                    codeList.push(temp);
+                    channel.send("/claimpromotion " + temp);
+                }
+            }
         }
     });
 }
